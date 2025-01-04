5baec3bcab2e5e33e1a09ca936286d2cbfd58325
Rohail Taimour <rohail.taimour@gmail.com>
2025-01-04 19:48:46 +0500

    another version

--- launch_vim.sh
+++ launch_vim.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
+# Check if VIM_CONFIG_DIR is provided
+if [ -z "$1" ]; then
+  echo "Usage: $0 <VIM_CONFIG_DIR> [APP_CODE_DIR]" >&2
+  exit 1
+fi
+
+VIM_CONFIG_DIR=$1
+
+# Application code directory (default to current directory if no argument is provided)
+APP_CODE_DIR=${2:-$(pwd)}
+
 if [ ! -d "$VIM_CONFIG_DIR" ]; then
   echo "Error: VIM_CONFIG_DIR does not exist: $VIM_CONFIG_DIR" >&2
   exit 1
 fi
 
-# Application code directory (default to current directory if no argument is provided)
-APP_CODE_DIR=${1:-$(pwd)}
-
 docker run --rm -it \
   -v "$APP_CODE_DIR":/workspace \
   -v "$VIM_CONFIG_DIR/.vim":/root/.vim \
-  -v "$HOME/.vim/autoload":/root/.vim/autoload \
-  -v "$HOME/.vim/plugged:"/root/.vim/plugged \
   -v "$VIM_CONFIG_DIR/.vimrc":/root/.vimrc \
    -w /workspace \
   custom-vim
