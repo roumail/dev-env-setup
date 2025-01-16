c0c0e245c500ffb0699e24947b56c09d7961b0fa
Rohail Taimour <rohail.taimour@gmail.com>
2025-01-16 16:38:24 +0500

    update entrypoint to reference updated plug file path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 set -e
 
-INIT_FILE="/root/.vim/config/plugins/init.vim"
+PLUGINS_FILE="/root/.vim/custom/plug.vim"
 
-if [ -f "$INIT_FILE" ]; then
-    echo "Found $INIT_FILE. Installing Vim plugins..."
+if [ -f "$PLUGINS_FILE" ]; then
+    echo "Found $PLUGINS_FILE. Installing Vim plugins..."
     # Run vim-plug in headless mode to install plugins
     # vim -E: Enables "Ex mode," which is non-interactive.
     # </dev/null: Prevents Vim from waiting for input during the build.
     # Ensure the container installs plugins into the host-mounted directory at runtime
-    vim -E -u "$INIT_FILE" +PlugInstall +qall </dev/null || {
+    vim -E -u "$PLUGINS_FILE" +PlugInstall +qall </dev/null || {
         echo "Vim plugin installation failed!" >&2
         exit 1
     }
 else
-    echo "Vim initialization file not found at $INIT_FILE. Skipping plugin installation."
+    echo "Vim initialization file not found at $PLUGINS_FILE. Skipping plugin installation."
 fi
 
 # Launch a Bash session to keep the container running
